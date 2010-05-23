6c85d27e4bbcfa02e844ae40daed629c5b51cdd9
Kevin Mark <kmark937@gmail.com>
2010-05-23 02:38:46 +0000

    Fixed FPD Exploit in output.php reported here: http://code.google.com/p/thehostingtool/issues/detail?id=2

--- includes/output.php
+++ includes/output.php
@@ -6,6 +6,9 @@
 // Released under the GNU-GPL
 //////////////////////////////
 
+//Check if called by script
+if(!defined("THT")){die();}
+
 $data = ob_get_contents(); # Get all the HTML created by the script
 ob_end_clean(); # Erase that data
 echo $style->prepare($data); # Prepare and output it
