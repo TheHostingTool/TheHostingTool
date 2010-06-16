b040aa91fdcb4fd3e6f286085c1dcfdd364618d8
Kevin Mark <kmark937@gmail.com>
2010-06-16 08:38:09 +0000

    Fixed problem in issue 12. Client Invoices weren't displaying correctly.

--- client/pages/invoices.php
+++ client/pages/invoices.php
@@ -2,7 +2,7 @@
 //////////////////////////////
 // The Hosting Tool
 // Client Area - Invoice Management
-// By Jonny H
+// By Jonny H and Kevin M
 // Released under the GNU-GPL
 //////////////////////////////
 
@@ -29,6 +29,12 @@ class page {
 		}
 		// List invoices. :)
 		$query = $db->query("SELECT * FROM `<PRE>invoices` WHERE `uid` = '{$_SESSION['cuser']}' ORDER BY `id` ASC");
+		$userdata = mysql_fetch_row($db->query("SELECT `user`,`firstname`,`lastname` FROM `<PRE>users` WHERE `id` = {$_SESSION['cuser']}"));
+		$domain = mysql_fetch_row($db->query("SELECT `domain` FROM `<PRE>user_packs` WHERE `userid` = {$_SESSION['cuser']}"));
+		$extra = array(
+			"userinfo" 	=> "$userdata[2], $userdata[1] ($userdata[0])",
+			"domain"	=> $domain[0]
+		);
 		$array2['list'] = "";
 		while($array = $db->fetch_array($query)){
 			$array['due'] = strftime("%D", $array['due']);
@@ -38,7 +44,7 @@ class page {
 			'<input type="button" name="pay" id="pay" value="Pay Now" onclick="doswirl(\''.$array['id'].'\')" />' :
 			'');
 			$array['amount'] = $array['amount']." ".$db->config("currency");
-			$array2['list'] .= $style->replaceVar("tpl/invoices/invoice-list-item.tpl", $array);
+			$array2['list'] .= $style->replaceVar("tpl/invoices/invoice-list-item.tpl", array_merge($array, $extra));
 		}
 		$array2['num'] = mysql_num_rows($query);
 		echo $style->replaceVar("tpl/invoices/client-page.tpl", $array2);
