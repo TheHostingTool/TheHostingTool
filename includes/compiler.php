fd97b05fd7e81d7e0706912f9c8d6f4721d1dad9
Nick Depinet <depinetnick@gmail.com>
2010-11-24 14:26:47 +0000

    THT now automatically re-directs to the install page if install = 0 in compiler.php

--- includes/compiler.php
+++ includes/compiler.php
@@ -87,11 +87,17 @@ foreach($_GET as $key => $value) {
 $path = dirname($_SERVER['PHP_SELF']);
 $position = strrpos($path,'/') + 1;
 define("FOLDER", substr($path,$position)); # Add current folder name to global
-if(FOLDER != "install" && FOLDER != "includes" && INSTALL != 1) { # Are we installing?	
-	$error['Error'] = "THT isn't Installed!";
-	$error['What to do'] = "Please run the install script @ <a href='".LINK."../install'>here</a>";
-	die($main->error($error));
+if(FOLDER != "install" && FOLDER != "includes" && INSTALL != 1) { # Are we installing?  
+                # Old Method- Uncomment if having trouble installing
+        //$error['Error'] = "THT isn't Installed!";
+        //$error['What to do'] = "Please run the install script @ <a href='".LINK."../install'>here</a>";
+        //die($main->error($error));
+        
+                #Lets just redirect to the installer, shall we?
+        $installURL = LINK . "../install";
+        header("Location: $installURL");
 }
+
 
 //Resets the error.
 $_SESSION['ecount'] = 0;
