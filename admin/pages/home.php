858f6f6d4c6cd5743ac123af9db97479f3bc5356
Nick Depinet <depinetnick@gmail.com>
2010-11-24 18:22:51 +0000

    changed the permissions on the conf.inc.php checker to look for 644 (better) rather than 444.

--- admin/pages/home.php
+++ admin/pages/home.php
@@ -39,8 +39,8 @@ class page {
 	
 	public function checkPerms($file){
 		$filechk = substr(sprintf('%o', fileperms($file)), -3);
-		if ($filechk != 444){
-			return "<div class='warn'><img src='../themes/icons/error.png' alt='' /> Warning: Configuration file (conf.inc.php) is still writable, please chmod it to 444!</div>";
+		if ($filechk != 644){
+			return "<div class='warn'><img src='../themes/icons/error.png' alt='' /> Warning: Configuration file (conf.inc.php) is still writable, please chmod it to 644!</div>";
 		}
 		else{
 			return "";
