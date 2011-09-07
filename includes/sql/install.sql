8825dbe8f9df7f50640c4f764b57e10cbca739a4
Kevin Mark <kmark937@gmail.com>
2011-09-07 03:29:30 +0000

    Reverted all Custom Order Field functionality. 1.2.4 is now on track. http://thehostingtool.com/forum/thread-1547.html

--- includes/sql/install.sql
+++ includes/sql/install.sql
@@ -540,19 +540,9 @@ CREATE TABLE IF NOT EXISTS `%PRE%logs` (
 -- Dumping data for table `%PRE%user_packs_bak`
 --
 
--- UPDATES
 
-ALTER TABLE `%PRE%users`  ADD `extra` LONGTEXT NOT NULL;
-INSERT INTO `%PRE%acpnav` (`id`, `visual`, `icon`, `link`) VALUES (NULL, 'Order Form', 'table.png', 'orderform');
-ALTER TABLE `%PRE%packages` ADD `custom_fields` TEXT NOT NULL;
-CREATE TABLE IF NOT EXISTS `%PRE%orderfields` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `order` int(11) NOT NULL DEFAULT '0',
-  `title` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `default` text NOT NULL,
-  `description` text NOT NULL,
-  `required` tinyint(1) NOT NULL DEFAULT '0',
-  `regex` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+-- --------------------------------------------------------
+
+
+ALTER TABLE `%PRE%user_packs` CHANGE `id` `id` MEDIUMINT( 9 ) UNSIGNED NOT NULL AUTO_INCREMENT ,
+CHANGE `userid` `userid` MEDIUMINT( 9 ) UNSIGNED NOT NULL;
--- includes/tpl/aorderform/bottom.tpl
+++
@@ -1,5 +0,0 @@
-<div id="atBottomDiv" style="text-align: center;">
-	<div id="saveOrderChangesDiv" class="hiddenStyle"><button id="saveOrderChangesBtn">Save Order Changes</button></div>
-	<div id="goodSavedDiv" class="savedDiv hiddenStyle">The order of your fields have been saved!</div>
-	<div id="badSavedDiv" class="savedDiv hiddenStyle">There was a problem when I tried to save the order of your fields...</div>
-</div>
