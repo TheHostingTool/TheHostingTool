595eb4187b8582f7e61b21f63b9bf3bcb5b6d2b1
Kevin Mark <kmark937@gmail.com>
2014-08-21 23:18:39 -0400

    Fix a rendering bug on the View Servers page
    Not certain what the purpose of this ever was.

--- admin/pages/servers.php
+++ admin/pages/servers.php
@@ -118,10 +118,6 @@ class page {
                         echo "<ERRORS>";
                         while($data = $db->fetch_array($query)) {
                             echo $main->sub("<strong>".$data['name']."</strong>", '<a href="?page=servers&sub=view&do='.$data['id'].'"><img src="'. URL .'themes/icons/magnifier.png"></a>');
-                            if($n) {
-                                echo "<br />";
-                            }
-                            $n++;
                         }
                     }
                 }
